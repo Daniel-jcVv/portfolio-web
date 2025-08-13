9d1bdf312daf9e068c3b72b9a8814a7586d422d0
Daniel-jcVv <daniel_sd6@proton.me>
2025-08-13 14:03:42 -0600

    (feat): Modify color index.css and beahvier StarBackground.jsx

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -82,8 +82,8 @@ export const StarBackground = () => {
                 key={meteor.id}
                 className="meteor animate-meteor" 
                 style={{
-                    width: meteor.size * 50  + "px",
-                    height: meteor.size * 2 + "px",
+                    width: meteor.size   + "px",
+                    height: meteor.size  + "px",
                     left: meteor.x + "%",
                     top: meteor.y + "%",
                     animationDelay: meteor.delay, 
